aabaab7f47dae92bb7f882347201a1bf9df5d637
Tim Batty <tbatty7@gmail.com>
2022-02-15 12:32:20 -0500

    Added table with 3 rows named one, two three

---
+++ TableView/MyTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MyTableViewController.swift
+//  TableView
+//
+//  Created by Timothy D Batty on 2/15/22.
+//
+
+import UIKit
+
+class MyTableViewController: UITableViewController {
+
+    private let model = ["One", "Two", "Three"]
+       
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        model.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mycell", for: indexPath)
+        cell.textLabel?.text = model[indexPath.row]
+                
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(model[indexPath.row])
+    }
+
+}
